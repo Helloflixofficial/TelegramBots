f958c7d75f761b656f841a3e64a64e1a8c6351d6
Ɓʘɓƴ <shboby71@gmail.com>
2024-01-13 23:23:23 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,25 @@
+import re, os
+
+id_pattern = re.compile(r'^.\d+$') 
+
+API_ID = os.environ.get("API_ID", "")
+
+API_HASH = os.environ.get("API_HASH", "")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+
+FORCE_SUB = os.environ.get("FORCE_SUB", "BJ") 
+
+         
+
+DB_NAME = os.environ.get("DB_NAME", "renamevjbot")     
+
+DB_URL = os.environ.get("DB_URL", "")
+ 
+FLOOD = int(os.environ.get("FLOOD", "10"))
+
+START_PIC = os.environ.get("START_PIC", "https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg")
+
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5606411877').split()]
+
+PORT = os.environ.get("PORT", "8080")
